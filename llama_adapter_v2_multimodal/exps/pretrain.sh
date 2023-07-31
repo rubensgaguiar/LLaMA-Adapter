804e2de338fbdf464c2a4f98125b8d9a983d4c3e
rubens <rubens.miguek@gmail.com>
2023-07-31 09:18:03 -0300

    sh path and torch inf

--- llama_adapter_v2_multimodal/exps/pretrain.sh
+++ llama_adapter_v2_multimodal/exps/pretrain.sh
@@ -6,9 +6,10 @@ OUTPUT_DIR="$3"
 
 mkdir -p "$OUTPUT_DIR"
 
-python -u -m torch.distributed.launch --master_port=1112 --nproc_per_node=8 --use_env \
+/opt/conda/bin/python -u -m torch.distributed.launch --master_port=1112 --nproc_per_node=8 --use_env \
  main_pretrain.py --data_config "$CONFIG" --batch_size 1 \
  --epochs 3 --split_epoch 1 --warmup_epochs 1 --blr 1.0e-4 --weight_decay 0.05 \
  --llama_path "$LLAMA_PATH" \
  --output_dir "$OUTPUT_DIR" \
  &>> "$OUTPUT_DIR"/output.log &
+
---
+++ llama_adapter_v2_multimodal/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+/opt/conda/bin/python -m pip install -r /root/LLaMA-Adapter/llama_adapter_v2_multimodal/requirements.txt --verbose --no-cache-dir
+
